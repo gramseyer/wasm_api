37bb19a7a578d5e1e6d5d3c22a1aff5ea53d6c54
Geoff Ramseyer <geoff.ramseyer@cs.stanford.edu>
2024-04-22 15:45:54 -0700

    fix req'd template instantiations

--- wasm_api/wasm_api.cc
+++ wasm_api/wasm_api.cc
@@ -147,7 +147,6 @@ LINK_DECL(uint64_t, uint32_t, uint32_t)
 
 LINK_DECL(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t)
 
-
 template void WasmRuntime::_link_fn<uint32_t>(                
     const char* module, const char* fn_name, uint32_t (*f)(void*));  
 template void WasmRuntime::_link_fn<int32_t>(                 
